ef2df396f8979f2de514b11537bc2657568bdaf6
Jan Musial <s28977@pjwstk.edu.pl>
2026-04-18 10:44:47 +0200

    Copied partial solution from tutorial video

---
+++ apbd-task5-asp.net-core-web-api-s28977/apbd-task5-asp.net-core-web-api-s28977/Controllers/RoomsController.cs
@@ -0,0 +1,53 @@
+﻿using apbd_task5_asp.net_core_web_api_s28977.Database;
+using apbd_task5_asp.net_core_web_api_s28977.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apbd_task5_asp.net_core_web_api_s28977.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<List<Room>> GetAll([FromQuery] int? minCapacity)
+    {
+        var rooms = DataStore.Rooms.AsEnumerable();
+
+        if (minCapacity.HasValue)
+        {
+            rooms = rooms.Where(r => r.Capacity >= minCapacity.Value);
+        }
+
+        return Ok(rooms.ToList());
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<Room> GetById([FromRoute] int id)
+    {
+        var room = DataStore.Rooms.FirstOrDefault(r => r.Id == id);
+        if (room == null)
+        {
+            return NotFound($"Room with id {id} not found");
+        }
+
+        return Ok(room);
+    }
+
+    [HttpPost]
+    public ActionResult<Room> CreateRoom([FromBody] Room room)
+    {
+        room.Id = DataStore.NextRoomId;
+        DataStore.Rooms.Add(room);
+
+        return CreatedAtAction(nameof(GetById), new { id = room.Id }, room);
+    }
+
+    [HttpGet("building/{buildingCode}")]
+    public ActionResult<List<Room>> GetByBuilding([FromRoute] string buildingCode)
+    {
+        var rooms = DataStore.Rooms
+            .Where(r => r.BuildingCode.Equals(buildingCode, StringComparison.OrdinalIgnoreCase))
+            .ToList();
+        return Ok(rooms);
+    }
+}
---
+++ apbd-task5-asp.net-core-web-api-s28977/apbd-task5-asp.net-core-web-api-s28977/Models/Reservation.cs
@@ -0,0 +1,6 @@
+﻿namespace apbd_task5_asp.net_core_web_api_s28977.Models;
+
+public class Reservation
+{
+    
+}
